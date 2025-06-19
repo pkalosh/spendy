5e560103e654984fee387077575eb63f776593e5
Patrick <mutuku.patrickk@gmail.com>
2025-06-19 18:00:38 +0300

    db setup

--- spendy/settings.py
+++ spendy/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'spendy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'spendy',
-        'USER': 'root',
-        'PASSWORD': 'Password123!',
-        'HOST': 'localhost',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '3306',
     }
 }
